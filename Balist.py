1e67b7614230dff2c852cb1d47ad8d4336735a51
lexandor <plex-team@mail.ru>
2019-04-01 00:36:50 +0400

    new formula y(x)

--- Balist.py
+++ Balist.py
@@ -28,20 +28,25 @@ speedStart.grid( row=1, column=0)
 speedStartEntry = Entry(varListFrame)
 speedStartEntry.grid( row=1, column=1)
 
+time = Label(varListFrame, text="t =")
+time.grid( row=2, column=0)
+timeEntry = Entry(varListFrame)
+timeEntry.grid( row=2, column=1)
+
 height = Label(varListFrame, text="h =")
-height.grid( row=2, column=0)
+height.grid( row=3, column=0)
 heightEntry = Entry(varListFrame)
-heightEntry.grid( row=2, column=1)
+heightEntry.grid( row=3, column=1)
 
 mass = Label(varListFrame, text="m =")
-mass.grid( row=3, column=0)
+mass.grid( row=4, column=0)
 massEntry = Entry(varListFrame)
-massEntry.grid( row=3, column=1)
+massEntry.grid( row=4, column=1)
 
 heightMax = Label(varListFrame, text="H =")
-heightMax.grid( row=4, column=0)
+heightMax.grid( row=5, column=0)
 heightMaxEntry = Entry(varListFrame)
-heightMaxEntry.grid( row=4, column=1)
+heightMaxEntry.grid( row=5, column=1)
 
 
 grafCanvas = Canvas(grafFrame, width=500, height=500, bg='white') 
@@ -58,32 +63,56 @@ x = 10
 y = 490
 
 def graf_dot():
-    global x
-    global y
-    print(" global y : " + str(y))
-    print(" global x : " + str(x))
-    Vo = 2
-    t = 60
-    a = 60
-    g = 10
+    #global x
+    #global y
+    #print(" global y : " + str(y))
+    #print(" global x : " + str(x))
 
-    k = 1
+    #== Получение значений из полей
+    #Vo = speedStartEntry.get()
+    #t  = timeEntry.get()
+    #m  = massEntry.get()
+    #h  = heightEntry.get()
+    #H  = heightMaxEntry.get()
+    #a  = angleEntry.get()
 
+    Vo = 20
+    t  = 60
+    a  = 60
+    g  = 10
+
+    
+    L = (((Vo**2)*sin(2*a))/g) # Длинна полета
+    k = L/490 # Коэффицент отображения, нужен чтобя график уместился на холсте
+
+    #начало координат:
     #xo = 10
     #yo = 490
-    x = x + ( Vo * t * cos(a) )
-    y = y - ( Vo * t * sin(a) ) + ( 0.5 * g * t * t)
+    
+    #formula -->  y = (tan(a)*x) - (g*(x**2))/(2*(Vo**2)*(cos(a)**2))
+ 
+
+    for x in range(10, 4910, 1):
+        y = ((tan(a)*x) - (g*(x**2))/(2*(Vo**2)*(cos(a)**2)))
+        y = 0-y
+        grafCanvas.create_oval(k * x + 10, k * y, k * x + 10, k * y, width=1, outline="black")
+        
     print(x)
     print(y)
+    
+    #grafCanvas.create_oval(k * x, k * y, k * x, k * y, width=1, outline="black")
 
-    grafCanvas.create_oval(k * x, k * y, k * x, k * y, width=1, outline="black")
+    #root.after(1, graf_dot)
 
-    root.after(5, graf_dot)
+def clearGraf():
+    grafCanvas.delete("all")
+    grafCanvas.create_line(10, 490, 490, 490, width=1, arrow=LAST) # X Ось
+    grafCanvas.create_line(10, 490, 10, 10, width=1, arrow=LAST) # Y Ось
 
 btnBuild = Button(controllerFrameLeft, text="Build", command=graf_dot)
 btnBuild.grid(row=0, column=0)
 
-btnClear = Button(controllerFrameLeft, text="Clear")
+btnClear = Button(controllerFrameLeft, text="Clear", command=clearGraf)
 btnClear.grid(row=0, column=1, padx=10)
 
 btnLoadGraf = Button(controllerFrameRight, text="loadGraf")
